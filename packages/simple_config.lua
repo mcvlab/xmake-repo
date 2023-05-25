e28b9f458474757f61194838d6a01ca8a8bc8ec7
jielyu <jiejielyu@outlook.com>
2023-05-25 11:07:15 +0800

    feat: 增加sample_config索引

---
+++ packages/simple_config.lua
@@ -0,0 +1,7 @@
+package("simple_config")
+    set_version("v0.0.1")
+    set_urls("git@github.com:mcvlab/simple_config.git")
+    -- add_deps("libc")
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
